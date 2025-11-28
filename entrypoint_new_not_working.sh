e3fbf486f10697d8f2b110cb7448c5c9f362152f
Alex Meier <planetlexx@gmail.com>
2025-11-28 16:27:43 +0100

    making my own image

---
+++ entrypoint_new_not_working.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# ComfyUI Docker Startup File by John Aldred
+# http://www.johnaldred.com
+# http://github.com/kaouthia
+
+set -e
+
+CN_DIR=/app/ComfyUI/custom_nodes
+INIT_MARKER="$CN_DIR/.custom_nodes_initialized"
+
+declare -A REPOS=(
+  ["ComfyUI-Manager"]="https://github.com/ltdrdata/ComfyUI-Manager.git"
+  ["ComfyUI_essentials"]="https://github.com/cubiq/ComfyUI_essentials.git"
+  ["ComfyUI-Crystools"]="https://github.com/crystian/ComfyUI-Crystools.git"
+  ["rgthree-comfy"]="https://github.com/rgthree/rgthree-comfy.git"
+  ["ComfyUI-KJNodes"]="https://github.com/kijai/ComfyUI-KJNodes.git"
+  ["ComfyUI_UltimateSDUpscale"]="https://github.com/ssitu/ComfyUI_UltimateSDUpscale.git"
+)
+
+# 1) Clone only once
+if [ ! -f "$INIT_MARKER" ]; then
+  echo "↳ First run: initializing custom_nodes…"
+  mkdir -p "$CN_DIR"
+  for name in "${!REPOS[@]}"; do
+    url="${REPOS[$name]}"
+    target="$CN_DIR/$name"
+    if [ -d "$target" ]; then
+      echo "  ↳ $name already exists, skipping clone"
+    else
+      echo "  ↳ Cloning $name"
+      git clone --depth 1 "$url" "$target"
+    fi
+  done
+  touch "$INIT_MARKER"
+fi
+
+# 2) Always ensure dependencies in the *current* venv
+echo "↳ Installing/upgrading dependencies…"
+for dir in "$CN_DIR"/*/; do
+  req="$dir/requirements.txt"
+  if [ -f "$req" ]; then
+    echo "  ↳ pip install --upgrade -r $req"
+    pip install --no-cache-dir --upgrade -r "$req"
+  fi
+done
+
+echo "↳ Launching ComfyUI"
+exec "$@"
+
